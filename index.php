1535b11d2fbd1451d09dde796439ba656e097b6b
Rastafanchik <r.a.s.t.a.f.a.n.c4.i.k@gmail.com>
2021-07-29 20:36:08 +0300

    changed file

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 
 echo '1. Создать функцию определяющую какой тип данных ей передан и выводящей соответствующее сообщение:' . '<br>';
 echo '<br>';
-echo 'Variant 1' . '<br>';
-echo '<br>';
 
 function checkDataType($val) {
   $res = gettype($val);
@@ -22,40 +20,6 @@ echo '6. ' . checkDataType([]) . '<br>';
 echo '7. ' . checkDataType(new stdClass()) . '<br>';
 echo '8. ' . checkDataType(fopen("foo", "w")) . '<br>';
 echo '9. ' . checkDataType(NULL) . '<br>';
-
-echo '<br>';
-echo 'Variant 2' . '<br>';
-echo '<br>';
-
-function checkDataType2($val) {
-  if (is_int($val)) { // is_integer, is_long
-    return 'integer';
-  } else if (is_float($val)) { // is_double, is_real
-    return 'float';
-  } else if (is_string($val)) {
-    return 'string';
-  } else if (is_bool($val)) {
-    return 'boolean';
-  } else if (is_array($val)) {
-    return 'array';
-  } else if (is_object($val)) {
-    return 'object';
-  } else if (is_resource($val)) {
-    return 'resource';
-  } else if (is_null($val)) {
-    return 'NULL';
-  }
-}
-
-echo '1. ' . checkDataType2(100) . '<br>';
-echo '2. ' . checkDataType2(100.1) . '<br>';
-echo '3. ' . checkDataType2('string') . '<br>';
-echo '4. ' . checkDataType2(true) . '<br>';
-echo '5. ' . checkDataType2(false) . '<br>';
-echo '6. ' . checkDataType2([]) . '<br>';
-echo '7. ' . checkDataType2(new stdClass()) . '<br>';
-echo '8. ' . checkDataType2(fopen("foo", "w")) . '<br>';
-echo '9. ' . checkDataType2(NULL) . '<br>';
 
 echo '<br>';
 echo '<hr>';
@@ -148,13 +112,13 @@ echo '<hr>';
 echo '4. Создать функцию которая определит сколько квадратов меньшего размера можно вписать в квадрат большего размера размер возвращать в float:' . '<br>';
 echo '<br>';
 
-function squaring($sqrFirst, $sqrSecond) {
+function squaring($sqrFirst, $sqrSecond) : float {
   $res = 0;
 
   if ($sqrFirst > $sqrSecond){
-    $res = $sqrFirst / $sqrSecond;
+    $res = ($sqrFirst * $sqrFirst) / ($sqrSecond * $sqrSecond);
   } else {
-    $res = $sqrSecond / $sqrFirst;
+    $res = ($sqrSecond * $sqrSecond) / ($sqrFirst * $sqrFirst);
   }
 
   return $res;
