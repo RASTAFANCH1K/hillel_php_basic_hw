cb0fc3d16701b1693feb964de7b348565d479c60
Rastafanchik <r.a.s.t.a.f.a.n.c4.i.k@gmail.com>
2021-08-10 19:14:11 +0300

    deleted comments

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
 use Core\Router;
+
 $router = new Router();
+
 $router->run();
-
-// require_once 'vendor/autoload.php';
-
-// use Core\Router;
-
-// $router = new Router();
-
-// echo $router->run();
